4d808ccd5d66082b9e738efb5b904646a555d13c
Baten <chepitolele@gmail.com>
2024-10-19 01:40:46 -0600

    Se agregó el proyecto

---
+++ app/Config/Routes.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/**
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(false);
+
+/**
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
+$routes->get('/', 'Home::index');
+$routes->get("articles", "Articles::index");
+$routes->get('articles/(:num)', 'Articles::show/$1');
+$routes->get("articles/new" , "Articles::new", ["as" => "new_article"]);
+$routes->post("articles/", "Articles::create");
+$routes->get("articles/(:num)/edit", "Articles::edit/$1");
+$routes->patch("articles/(:num)", "Articles::update/$1");
+$routes->match(["get", "delete"], "articles/delete/(:num)", "Articles::delete/$1");
+$routes->get("articles/(:num)/delete", "Articles::confirmDelete/$1");
+
+/**
+ * --------------------------------------------------------------------
+ * Additional Routing
+ * --------------------------------------------------------------------
+ *
+ * There will often be times that you need additional routing and you
+ * need to it be able to override any defaults in this file. Environment
+ * based routes is one such time. require() additional route files here
+ * to make that happen.
+ *
+ * You will have access to the $routes object within that file without
+ * needing to reload it.
+ */
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
